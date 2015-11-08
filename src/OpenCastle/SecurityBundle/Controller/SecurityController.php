4117dd86bec10c8ac81f353c369423f04044d378
Lucio Merotta <l.merotta@gmail.com>
2015-11-08 22:16:54 +0100

    Début du design de la homepage + formulaires

--- src/OpenCastle/SecurityBundle/Controller/SecurityController.php
+++ src/OpenCastle/SecurityBundle/Controller/SecurityController.php
@@ -2,7 +2,9 @@
 
 namespace OpenCastle\SecurityBundle\Controller;
 
+use OpenCastle\SecurityBundle\Form\Type\Player\InscriptionFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class SecurityController
@@ -10,4 +12,42 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  */
 class SecurityController extends Controller
 {
+
+    public function inscriptionAction(Request $request)
+    {
+        $playerManager = $this->get('opencastle_security.player_manager');
+
+        $player = $playerManager->createPlayer();
+
+        $form = $this->createForm(new InscriptionFormType(), $player);
+
+        if($request->getMethod() === 'POST')
+        {
+            $form->handleRequest($request);
+
+            if($form->isValid())
+            {
+                // ajout du user au groupe de base
+                $entityManager = $this->getDoctrine()->getManager();
+
+                $playerManager->updatePlayer($player);
+            }
+        }
+
+        // retourner la vue
+    }
+
+    public function connexionAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        // retourner la vue
+    }
+
 }
